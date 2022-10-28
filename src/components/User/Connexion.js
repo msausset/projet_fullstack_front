5e817094d46f6cdb467fd35a1360bdf7af49bcd4
viverk <viverkange@live.fr>
2022-10-28 17:05:28 +0200

    First commit : Navbar

---
+++ src/components/User/Connexion.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Connexion = () => {
+  return (
+    <div>
+      <div className='w-2/3 bg-black -mt-10 min-h-screen m-auto'>
+
+      <p className='text-4xl  text-center'>Connexion</p>
+      </div>
+    </div>
+  )
+}
+
+export default Connexion
